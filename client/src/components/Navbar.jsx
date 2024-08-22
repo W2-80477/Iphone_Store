8a19a80de9d1d0cdc473d20aa5eb2952a203c019
W2-80477 <satyamkumar.sk786@gmail.com>
2024-08-22 14:36:09 +0530

    iphone Store

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+function Navbar() {
+    const navigate = useNavigate()
+
+    
+    const cart = useSelector(state => state.cart)
+
+    const onLogout = () => {
+        sessionStorage.removeItem('token')
+        navigate('/')
+    }
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg " style={{ backgroundColor: "lightgray" }}>
+                <div style={{ display: "flex" }} className="container-fluid">
+                    <Link className="navbar-brand" to='/home' >iPhone Store</Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+                            <Link className="nav-link" aria-current="page" to="/home">
+                                Home
+                            </Link>
+                            <Link className="nav-link" aria-current="page" to='/cart'>
+                                Cart ({cart.items.length})
+                            </Link>
+                            <Link className="nav-link" aria-current="page" to='/orders'>
+                                Orders
+                            </Link>
+
+                            <div style={{ justifyContent: "flex-end" }}>
+                                <button onClick={onLogout} className="nav-link" aria-current="page" >
+                                    Logout
+                                </button>
+                            </div>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { getAllIphone } from "../services/iphone";
+import { toast } from "react-toastify";
+import config from "../config"
+import Footer from "../components/Footer";
+import { useDispatch } from "react-redux";
+import { addItem } from "../features/cartSlice";
+import { useNavigate } from "react-router-dom";
+
+function Iphone({ item }) {
+
+    
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const addItemToCart = () => {
+        dispatch(addItem({ ...item, quantity: 1 }))
+        navigate('/cart')
+    }
+    return (
+        <div style={{ borderRadius: "30px" }} className="card m-1">
+            <div style={{ textAlign: "center" }}>
+                <img
+                    style={{ width: "150px", margin: "15px" }}
+                    className='card-img-top'
+                    src={config.server + '/' + item.image}
+                    alt="" />
+            </div>
+            <div className="card-body">
+                <div style={{ fontWeight: "bold", fontSize: "19px" }}>{item.title}</div>
+                <div>Price: <span style={{ fontWeight: "bold", fontSize: "17px" }}>₹{item.price}</span></div>
+                <div onClick={addItemToCart} className="btn btn-success btn-sm mt-2">Add to cart</div>
+            </div>
+        </div>
+    )
+}
+
+function Home() {
+    const [items, setItems] = useState([])
+    const loadAllIphones = async () => {
+        const result = await getAllIphone()
+        if (result['status'] == 'success') {
+            console.log(result)
+            setItems(result['data'])
+        }
+        else {
+            toast.error(result['error'])
+        }
+    }
+
+    useEffect(() => {
+        loadAllIphones()
+    }, [])
+
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <h1 className="title">iPhone</h1>
+                <div className="row">
+                    {items.map((item) => {
+                        return (
+                            <div key={item['id']} className="col-4">
+                                <Iphone item={item}
+                                />
+                            </div>
+                        )
+                    })}
+                </div>
+
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
